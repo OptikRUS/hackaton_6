e98b08e75470613c42ac9a88316ce1677230c1c5
OptikRUS <optikrus61@gmail.com>
2024-06-06 18:40:53 +0300

    added users fixtures

---
+++ src/api/import_data/import_fixtures.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Response, status
+
+from src.core.trainings.fixtures.exercise import fill_exercises
+from src.core.trainings.fixtures.training_types import fill_training_types
+from src.core.users.fixtures.clients import fill_clients, fill_trainers
+
+router = APIRouter(prefix="/import", tags=["import"])
+
+
+@router.get("/data", status_code=status.HTTP_200_OK)
+async def import_data() -> Response:
+    await fill_exercises()
+    await fill_training_types()
+    return Response(status_code=status.HTTP_200_OK)
+
+
+@router.get("/clients/{count}", status_code=status.HTTP_200_OK)
+async def import_clients(count: int) -> Response:
+    await fill_clients(count=count)
+    return Response(status_code=status.HTTP_200_OK)
+
+
+@router.get("/trainers/{count}", status_code=status.HTTP_200_OK)
+async def import_trainers(count: int) -> Response:
+    await fill_trainers(count=count)
+    return Response(status_code=status.HTTP_200_OK)
--- src/api/routers.py
+++ src/api/routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from src.api.health import health
-from src.api.import_data.import_exercises import router as exercise_router
+from src.api.import_data.import_fixtures import router as import_router
 from src.core.chats.api.messages import router as chats_router
 from src.core.media.api.media import router as s3_router
 from src.core.trainings.api.trainings import router as trainings_router
@@ -9,8 +9,8 @@ from src.core.users.api.users import router as users_router
 
 root_router = APIRouter()
 root_router.include_router(health.router, include_in_schema=False)
-root_router.include_router(exercise_router, include_in_schema=True)
 root_router.include_router(users_router, include_in_schema=True)
 root_router.include_router(trainings_router, include_in_schema=True)
 root_router.include_router(chats_router, include_in_schema=True)
 root_router.include_router(s3_router, include_in_schema=True)
+root_router.include_router(import_router, include_in_schema=True)
