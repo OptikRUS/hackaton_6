f81c149358d495f31619014a33e31d8f501ffb14
Medvedko Kirill <optikrus61@gmail.com>
2024-06-16 22:51:31 +0300

    add content alias

--- src/core/chats/api/schemas/responses.py
+++ src/core/chats/api/schemas/responses.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
+from pydantic import Field
+
 from src.api.schemas.base_schemas import ApiModel
 
 
 class MessageResponse(ApiModel):
-    content: str | None = None
+    content: str | None = Field(alias="message")
     receiver_id: int
     sender_id: int
     url: str | None = None
